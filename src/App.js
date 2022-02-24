3b2380725763cc3db74fe98fe0e5989243e4d5bf
amin <aminqanbarii1992@gmail.com>
2022-02-24 03:53:29 -0800

    add footer and footer icons and styles

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Products from "./components/Products";
 import { GlobalStyle } from "./globalStyles";
 import { productData , productDataTwo } from "./components/Products/data";
 import Feature from "./components/Feature";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
      <Products heading='Choose your favorite' data={productData}/>
      <Feature/>
      <Products heading='Sweet Treats for you' data={productDataTwo}/>
+     <Footer/>
     </div>
   );
 }
