c0c9a9b0454e197f688f0172c561c5b976d68ef2
amin <aminqanbarii1992@gmail.com>
2022-02-24 03:16:01 -0800

    i add products ,data and styles from PorductsElements

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import Hero from "./components/Hero";
+import Products from "./components/Products";
 import { GlobalStyle } from "./globalStyles";
+import { productData } from "./components/Products/data";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle/>
      <Hero/> 
+     <Products heading='Choose your favorite' data={productData}/>
     </div>
   );
 }
